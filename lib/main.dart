4203956814a7f72c85ccb802f2d886d8e07990fd
Tanmay Bharambe <bharambetr2002@gmail.com>
2024-01-27 13:08:32 +0530

    Add Firebase dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:farm2fabric/consts/consts.dart';
 import 'package:farm2fabric/authentication/view/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
