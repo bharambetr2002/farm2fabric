4974e4cfeb04e66c3c28e6cc47f828c4f582acd3
Divij Sahu <maildivij@gmail.com>
2024-01-31 00:19:36 +0530

    Keyboard dismiss and Internet permission

--- lib/authentication/view/login_screen.dart
+++ lib/authentication/view/login_screen.dart
@@ -16,101 +16,105 @@ class loginScreen extends StatelessWidget {
     var controller = Get.put(AuthControleer());
 
     return bgWidget(
-        child: Scaffold(
-            resizeToAvoidBottomInset: false,
-            body: Center(
-              child: Column(
-                children: [
-                  (context.screenHeight * 0.1).heightBox,
-                  applogoWidget(),
-                  10.heightBox,
-                  "Log in to $appname"
-                      .text
-                      .fontFamily(bold)
-                      .white
-                      .size(18)
-                      .make(),
-                  15.heightBox,
-                  Obx(
-                    () => Column(
-                      children: [
-                        customTextField(
-                            hint: emailHint,
-                            title: email,
-                            isPass: false,
-                            controller: controller.emailController),
-                        customTextField(
-                            hint: passwordHint,
-                            title: password,
-                            isPass: true,
-                            controller: controller.passwordController),
-                        Align(
-                          alignment: Alignment.centerRight,
-                          child: TextButton(
-                              onPressed: () {}, child: forgetPass.text.make()),
-                        ),
-                        5.heightBox,
-                        controller.isloading.value
-                            ? const CircularProgressIndicator(
-                                valueColor: AlwaysStoppedAnimation(redColor),
-                              )
-                            : ourButton(
-                                color: redColor,
-                                title: login,
-                                textColor: whiteColor,
-                                onPress: () async {
-                                  controller.isloading.value = true;
-                                  await controller
-                                      .loginMethod(context: context)
-                                      .then((value) {
-                                    if (value != null) {
-                                      VxToast.show(context, msg: loggedin);
-                                      Get.offAll(() => const Home_Customer());
-                                    } else {
-                                      controller.isloading.value = false;
-                                    }
-                                  });
-                                },
-                              ).box.width(context.screenWidth - 50).make(),
-                        5.heightBox,
-                        createNewAccount.text.color(fontGrey).make(),
-                        ourButton(
-                            color: golden,
-                            title: signup,
-                            textColor: whiteColor,
-                            onPress: () {
-                              Get.to(() => const SignupScreen());
-                            }).box.width(context.screenWidth - 50).make(),
-                        10.heightBox,
-                        loginWith.text.color(fontGrey).make(),
-                        5.heightBox,
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: List.generate(
-                                3,
-                                (index) => Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: CircleAvatar(
-                                        backgroundColor: lightGrey,
-                                        radius: 25,
-                                        child: Image.asset(
-                                          socialIconList[index],
-                                          width: 30,
-                                        ),
+        child: GestureDetector(
+      // to hide keyboard when user click outside the textfield
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          body: Center(
+            child: Column(
+              children: [
+                (context.screenHeight * 0.1).heightBox,
+                applogoWidget(),
+                10.heightBox,
+                "Log in to $appname"
+                    .text
+                    .fontFamily(bold)
+                    .white
+                    .size(18)
+                    .make(),
+                15.heightBox,
+                Obx(
+                  () => Column(
+                    children: [
+                      customTextField(
+                          hint: emailHint,
+                          title: email,
+                          isPass: false,
+                          controller: controller.emailController),
+                      customTextField(
+                          hint: passwordHint,
+                          title: password,
+                          isPass: true,
+                          controller: controller.passwordController),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                            onPressed: () {}, child: forgetPass.text.make()),
+                      ),
+                      5.heightBox,
+                      controller.isloading.value
+                          ? const CircularProgressIndicator(
+                              valueColor: AlwaysStoppedAnimation(redColor),
+                            )
+                          : ourButton(
+                              color: redColor,
+                              title: login,
+                              textColor: whiteColor,
+                              onPress: () async {
+                                controller.isloading.value = true;
+                                await controller
+                                    .loginMethod(context: context)
+                                    .then((value) {
+                                  if (value != null) {
+                                    VxToast.show(context, msg: loggedin);
+                                    Get.offAll(() => const Home_Customer());
+                                  } else {
+                                    controller.isloading.value = false;
+                                  }
+                                });
+                              },
+                            ).box.width(context.screenWidth - 50).make(),
+                      5.heightBox,
+                      createNewAccount.text.color(fontGrey).make(),
+                      ourButton(
+                          color: golden,
+                          title: signup,
+                          textColor: whiteColor,
+                          onPress: () {
+                            Get.to(() => const SignupScreen());
+                          }).box.width(context.screenWidth - 50).make(),
+                      10.heightBox,
+                      loginWith.text.color(fontGrey).make(),
+                      5.heightBox,
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: List.generate(
+                              3,
+                              (index) => Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: CircleAvatar(
+                                      backgroundColor: lightGrey,
+                                      radius: 25,
+                                      child: Image.asset(
+                                        socialIconList[index],
+                                        width: 30,
                                       ),
-                                    )))
-                      ],
-                    )
-                        .box
-                        .white
-                        .rounded
-                        .padding(const EdgeInsets.all(16))
-                        .width(context.screenWidth - 70)
-                        .shadowSm
-                        .make(),
+                                    ),
+                                  )))
+                    ],
                   )
-                ],
-              ),
-            )));
+                      .box
+                      .white
+                      .rounded
+                      .padding(const EdgeInsets.all(16))
+                      .width(context.screenWidth - 70)
+                      .shadowSm
+                      .make(),
+                )
+              ],
+            ),
+          )),
+    ));
   }
 }
