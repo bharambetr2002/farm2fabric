9acd66273e53439c2419f666a03115a49ff0bdf9
Tanmay Bharambe <bharambetr2002@gmail.com>
2024-01-31 20:41:03 +0530

    working safe - {logout}

--- lib/consts/consts.dart
+++ lib/consts/consts.dart
@@ -6,4 +6,3 @@ export './firebase_consts.dart';
 export 'package:velocity_x/velocity_x.dart';
 export 'package:flutter/material.dart';
 export 'package:get/get.dart';
-export './firebase_const.dart';
