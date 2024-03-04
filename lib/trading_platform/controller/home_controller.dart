8b76cec4f10f4831e2c138690982d705cab6ab2d
Tanmay Bharambe <bharambetr2002@gmail.com>
2024-03-04 19:10:05 +0530

    Basic Auth  Improved

--- lib/trading_platform/controller/home_controller.dart
+++ lib/trading_platform/controller/home_controller.dart
@@ -9,19 +9,19 @@ class HomeController extends GetxController {
 
   var currentNavIndex = 0.obs;
 
-  var username = '';
+  var username = ''.obs;
 
-  getUsername() async {
-    var n = await firestore
-        .collection(usersCollection)
-        .where('id', isEqualTo: currentUser!.uid)
-        .get()
-        .then((value) {
-      if (value.docs.isNotEmpty) {
-        return value.docs.single['name'];
-      }
-    });
-    username = n;
-    print(username);
-  }
+Future<void> getUsername() async {
+  var n = await firestore
+      .collection(usersCollection)
+      .doc(currentUser!.uid) // Use doc() instead of where() to directly fetch the document by ID
+      .get()
+      .then((value) {
+    if (value.exists) {
+      return value.data()!['name']; // Use data() to get the document data
+    }
+  });
+  username.value = n ?? 'Unknown'; // Update username with value from Firestore or default to 'Unknown'
+  print(username.value);
+}
 }
